a010b06a360f9e58dbb4697a40866ce697cd473e
Tyler H <tyler.r.herzog@gmail.com>
2023-07-05 12:27:50 -0700

    Created simple chat between client and server

--- client.py
+++ client.py
@@ -1,1 +1,19 @@
-import socket;
+import socket
+
+host = input("Enter server IP: ")
+
+HOST = host
+PORT = 12345
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    print("Connection Successful!\nStart chatting with your partner")
+    msg = input("Client: ")
+    while msg != "quit":
+        bmsg = bytes(msg, 'utf-8')
+        s.sendall(bmsg)
+        data = s.recv(1024)
+        print(f"Server: {data!r}")
+        msg = input("Client: ")
+
+print("Connection Closed")
--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-import socket;
+import socket
 
-HOST = ""  # Standard loopback interface address (localhost)
-PORT = 1234  # Port to listen on (non-privileged ports are > 1023)
+HOST = ""  # This should make it so that the server accepts any IP address
+PORT = 12345
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
-    
+    with conn:
+        print(f"Connected to {addr}")
+        data = conn.recv(1024)
+        while data:
+            print(f"Client: {data!r}")
+            msg = input("Server: ")
+            bmsg = bytes(msg, 'utf-8')
+            conn.sendall(bmsg)
+            data = conn.recv(1024)
+    print("Client has disconnected")
