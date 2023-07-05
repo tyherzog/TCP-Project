82645e5158b1c6179e014d03f11a7303e9c2354c
Tyler H <tyler.r.herzog@gmail.com>
2023-07-05 11:58:43 -0700

    created server client file

---
+++ server.py
@@ -0,0 +1,10 @@
+import socket;
+
+HOST = ""  # Standard loopback interface address (localhost)
+PORT = 1234  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    
