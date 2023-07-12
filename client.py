84d384daa08998371948c96cc8f8821c80a25206
Tyler <tyler.r.herzog@gmail.com>
2023-07-12 14:19:30 -0700

    Modified files to reflect the first msg and ack

--- client.py
+++ client.py
@@ -4,16 +4,21 @@ host = input("Enter server IP: ")
 
 HOST = host
 PORT = 12345
+slidingWindow = 1 # sliding window initialized to 1
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print("Connection Successful!\nStart chatting with your partner")
-    msg = input("Client: ")
-    while msg != "quit":
-        bmsg = bytes(msg, 'utf-8')
-        s.sendall(bmsg)
-        data = s.recv(1024)
-        print(f"Server: {data!r}")
-        msg = input("Client: ")
-
+    print("Connection Successful!\n")
+    s.sendall("network".encode('UTF-8')) # Send initial string
+    
+    # msg = input("Client: ")
+    # while msg != "quit":
+    #     s.sendall(msg.encode())
+    #     data = s.recv(1024)
+    #     print(f"Server: " + data.decode())
+    #     msg = input("Client: ")
+    data = s.recv(1024)
+    print(f"Server: " + data.decode('UTF-8'))
 print("Connection Closed")
+
+
--- server.py
+++ server.py
@@ -2,18 +2,27 @@ import socket
 
 HOST = ""  # This should make it so that the server accepts any IP address
 PORT = 12345
+windowSize = 1
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
+    print("Waiting for connection from a host...")
+    # while msg != "quit":
+    #     s.sendall(msg.encode())
+    #     data = s.recv(1024)
+    #     print(f"Server: " + data.decode())
+    #     msg = input("Client: ")
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print(f"Connected to {addr}")
+        print("Connected to ", str(addr))
         data = conn.recv(1024)
-        while data:
-            print(f"Client: {data!r}")
-            msg = input("Server: ")
-            bmsg = bytes(msg, 'utf-8')
-            conn.sendall(bmsg)
-            data = conn.recv(1024)
+        if data.decode('UTF-8') is "network":
+            msg = "ACK"
+            conn.sendall(msg.encode('UTF-8'))
+        # while data:
+        #     print("Client: " + data.decode())
+        #     msg = input("Server: ")
+        #     conn.sendall(msg.encode())
+        #     data = conn.recv(1024)
     print("Client has disconnected")
